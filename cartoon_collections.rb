13d8e1d4ac3e65854c76586d1470e96d824eee97
Jesse Langley <jlangleyiii@aol.com>
2020-04-12 18:06:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,7 +26,7 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
     planeteer_calls.find do |element| 
-      element == valid_calls.collect(element)
+      element == valid_calls.find(element)
     end  
          
        
